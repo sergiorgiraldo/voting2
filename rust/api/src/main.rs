8e63f66fcc9510a1f338afc9d33bf6d55260df99
Sergio Giraldo <sergio@giraldo.com.br>
2022-11-29 16:05:54 +0100

    endpoints added:: by sergio giraldo @ 20221129T1605CET, gpg signed

--- rust/api/src/main.rs
+++ rust/api/src/main.rs
@@ -1,31 +1,46 @@
 use firerust::FirebaseClient;
 use serde_json::Value;
-fn main() {
+use actix_web::{get, post, web, Result, App, HttpResponse, HttpServer, Responder};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new()
+        .service(get_users)
+        .service(get_user)
+        .service(get_polls)
+        .service(get_poll)
+        .service(vote_for_poll)
+    )
+    .bind(("localhost", 8080))?
+    .run()
+    .await
 }
 
-#[allow(dead_code)]
-fn get_users(){
+#[get("/users")]
+async fn get_users()-> impl Responder{
     let firebase = FirebaseClient::new("https://voting-509cd-default-rtdb.europe-west1.firebasedatabase.app/").unwrap();
     let users = firebase.reference("/User");
-    println!("{:?}", users.get::<Value>());
+    HttpResponse::Ok().json(format!("{:?}", users.get::<Value>().unwrap()))
 }
 
-#[allow(dead_code, unused)]
-fn get_user(user:&str){
-    
+#[get("/user/{user_id}")]
+async fn get_user(user: web::Path<i32>)-> impl Responder{
+    HttpResponse::Ok().json(format!("The user {}", user))
 }
 
-#[allow(dead_code)]
-fn get_polls(){
-    
+#[get("/polls")]
+async fn get_polls()-> impl Responder{
+    let firebase = FirebaseClient::new("https://voting-509cd-default-rtdb.europe-west1.firebasedatabase.app/").unwrap();
+    let polls = firebase.reference("/Poll");
+    HttpResponse::Ok().json(format!("{:?}", polls.get::<Value>().unwrap()))
 }
 
-#[allow(dead_code, unused)]
-fn get_poll(id:&u32){
-    
+#[get("/poll/{poll_id}")]
+async fn get_poll(poll: web::Path<i32>)-> impl Responder{
+    HttpResponse::Ok().json(format!("The poll {}", poll))
 }
 
-#[allow(dead_code, unused)]
-fn vote_for_poll(id_poll:&u32, id_question:&u32){
-    
+#[post("/vote/{poll_id}/{question_id}")]
+async fn vote_for_poll(vote: web::Path<(u32, u32)>)-> impl Responder{
+    HttpResponse::Ok().json(format!("Vote {} for {}!", vote.1, vote.0))
 }
